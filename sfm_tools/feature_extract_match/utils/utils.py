207db95055e5c0b8e0d8b375591c2566d863caf4
lihan1 <lihan1@senseauto.com>
2025-03-18 17:11:19 +0800

    feat: add entry SFM_IMG_PAIR_TIME_INTERVAL

--- sfm_tools/feature_extract_match/utils/utils.py
+++ sfm_tools/feature_extract_match/utils/utils.py
@@ -1,5 +1,16 @@
 import os
 
+env_value = os.getenv("SFM_IMG_PAIR_TIME_INTERVAL", "").strip()
+
+try:
+    SFM_IMG_PAIR_TIME_INTERVAL = float(env_value) if env_value else 2
+except ValueError:
+    print(f"Warning: Invalid SFM_IMG_PAIR_TIME_INTERVAL='{env_value}', using default value 2.")
+    SFM_IMG_PAIR_TIME_INTERVAL = 2
+
+print(f"SfM: SFM_IMG_PAIR_TIME_INTERVAL={SFM_IMG_PAIR_TIME_INTERVAL}")
+
+
 def get_img_pairs(img_fnames):
     index_pairs = []
     for i in range(len(img_fnames)):
@@ -9,9 +20,10 @@ def get_img_pairs(img_fnames):
             time0, _ = os.path.splitext(img_fnames[i].split("/")[-1])
             time1, _ = os.path.splitext(img_fnames[j].split("/")[-1])
             time0, time1 = float(time0)/1000, float(time1)/1000
-            if abs(time0-time1) <= 2.0:
+            if abs(time0-time1) <= SFM_IMG_PAIR_TIME_INTERVAL:
                 index_pairs.append((i, j))
     return index_pairs
+
 
 def remove_db(database_path):
     if os.path.isfile(database_path):
